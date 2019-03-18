6dc8f350cfb812c74d9cff5e24665eea18247214
liminghui <limh1993@dingtalk.com>
2019-03-18 17:22:48 +0800

    注释

--- src/main/java/org/spring/aop_demo/a/MyAspect.java
+++ src/main/java/org/spring/aop_demo/a/MyAspect.java
@@ -6,8 +6,12 @@ import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class MyAspect {
-	
+
 	// 限制方法匹配的字符串格式
+	// 第一个*：所有返回类型
+	// 第二个*：所有类
+	// 第三个*：所有方法
+	// ..表示任意入参类型和入参数量
 	@Before("execution(* org.spring.aop_demo.a.service.*.*(..))")
 	public void before() {
 		System.out.println("========== before");
